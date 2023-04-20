6275fb0e12c5e145e4177f26eb8db79560ed6d87
Abhishek Kumar <abhishekkumar.in047@gmail.com>
2023-04-20 13:50:23 +0530

    redux added and single state added

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,14 @@
 import '@/styles/globals.css';
+import Store from '@/components/Store';
+import { Provider } from 'react-redux';
+
 
 export default function App({ Component, pageProps }) {
   return(
+    <>
+      <Provider store={Store}>
     <Component {...pageProps} />
+      </Provider>
+    </>
      )
 };
